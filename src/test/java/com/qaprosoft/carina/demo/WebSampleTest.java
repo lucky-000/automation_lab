6ecadcfdaca19f26732684e70c9d1a121ed46faf
lucky-000 <myrchykandriy@gmail.com>
2021-03-04 19:24:30 +0200

    first test

--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -24,19 +24,8 @@ import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
-import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
-import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
-import com.qaprosoft.carina.core.foundation.utils.tag.TestTag;
-import com.qaprosoft.carina.demo.gui.components.FooterMenu;
-import com.qaprosoft.carina.demo.gui.components.NewsItem;
-import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs;
-import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs.SpecType;
-import com.qaprosoft.carina.demo.gui.pages.BrandModelsPage;
-import com.qaprosoft.carina.demo.gui.pages.CompareModelsPage;
 import com.qaprosoft.carina.demo.gui.pagesLab.HomePage;
 import com.qaprosoft.carina.demo.gui.pagesLab.components.SearchingItem;
-import com.qaprosoft.carina.demo.gui.pages.ModelInfoPage;
-import com.qaprosoft.carina.demo.gui.pages.NewsPage;
 
 /**
  * This sample shows how create Web test.
@@ -66,73 +55,7 @@ public class WebSampleTest extends AbstractTest {
 //	        	Assert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchingText), "Invalid search results!");
 //	        }
 
-//    @Test(description = "JIRA#AUTO-0008")
-//    @MethodOwner(owner = "qpsdemo")
-//    @TestPriority(Priority.P3)
-//    public void testModelSpecs() {
-//        // Open GSM Arena home page and verify page is opened
-//        HomePage homePage = new HomePage(getDriver());
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-//        
-//        //Closing advertising if it's displayed
-//        homePage.getWeValuePrivacyAd().closeAdIfPresent();
-//        
-//        // Select phone brand
-//        homePage = new HomePage(getDriver());
-//        BrandModelsPage productsPage = homePage.selectBrand("Samsung");
-//        // Select phone model
-//        ModelInfoPage productInfoPage = productsPage.selectModel("Galaxy S10 Lite");
-//        // Verify phone specifications
-//        Assert.assertEquals(productInfoPage.readDisplay(), "6.7\"", "Invalid display info!");
-//        Assert.assertEquals(productInfoPage.readCamera(), "48MP", "Invalid camera info!");
-//        Assert.assertEquals(productInfoPage.readRam(), "6/8GB RAM", "Invalid ram info!");
-//        Assert.assertEquals(productInfoPage.readBattery(), "4500mAh", "Invalid battery info!");
-//    }
-//
-//
-//    @Test(description = "JIRA#AUTO-0009")
-//    @MethodOwner(owner = "qpsdemo")
-//    @TestPriority(Priority.P1)
-//    @TestTag(name = "area test", value = "web")
-//    public void testCompareModels() {
-//        // Open GSM Arena home page and verify page is opened
-//        HomePage homePage = new HomePage(getDriver());
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-//        // Open model compare page
-//        FooterMenu footerMenu = homePage.getFooterMenu();
-//        Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
-//        CompareModelsPage comparePage = footerMenu.openComparePage();
-//        // Compare 3 models
-//        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
-//        // Verify model announced dates
-//        Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2016, March 31");
-//        Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June 19");
-//        Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
-//    }
-//    
-//    @Test(description = "JIRA#AUTO-0010")
-//    @MethodOwner(owner = "qpsdemo")
-//    public void testNewsSearch() {
-//        HomePage homePage = new HomePage(getDriver());
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-//        
-//        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
-//        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
-//        
-//        final String searchQ = "iphone";
-//        List<NewsItem> news = newsPage.searchNews(searchQ);
-//        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
-//        for(NewsItem n : news) {
-//            System.out.println(n.readTitle());
-//            Assert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ), "Invalid search results!");
-//        }
-//    }
-    
-   
-        
+  
         
     }
 
